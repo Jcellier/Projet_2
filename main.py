dad04130bbc9dfd3f3d093afb4ce59352478c2c5
Cellier-Auffret Jordan <jcellierauffret@lilo.org>
2020-11-17 09:55:20 +0100

    Ajout de la creation d'un fichier csv pour recueillir les infos scrape par le script

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """ Projet de Web Scraping avec BeautifulSoup sur le site bookstoscrape.com """
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 website_url = "books.toscrape.com"
 base_url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"  # Url qui va servir de base
@@ -9,32 +10,47 @@ base_url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.
 def get_books_info():
     """ Fonction qui va chercher toutes les infos spécifique à un livre"""
     r = requests.get(base_url)  # Requests de la page du livre
-    soup = BeautifulSoup(r.content, 'lxml')  # Parsing, mise en page de la page du livre
+    if r.status_code == 200:  # Si la page est accessible alors on continue
+        result = []  # Creation d'une liste vide pour y ajouter les infos du livres plus tard
+        soup = BeautifulSoup(r.content, 'lxml')  # Parsing, mise en page de la page du livre
 
-    upc = soup.find("table", class_="table table-striped").td.text  # Extraction de la donnée upc du livre
+        upc = soup.find("table", class_="table table-striped").td.text  # Extraction de la donnée upc du livre
 
-    title = soup.find("div", class_="col-sm-6 product_main").h1.text  # Extraction du titre du livre
+        title = soup.find("div", class_="col-sm-6 product_main").h1.text  # Extraction du titre du livre
 
-    price_incl_tax = soup.find("table", class_="table table-striped").\
-        find("th", text="Price (incl. tax)").nextSibling.text  # Extraction du prix TTC du livre
-    price_excl_tax = soup.find("table", class_="table table-striped").\
-        find("th", text="Price (excl. tax)").nextSibling.text  # Extraction du prix HT du livre
+        price_incl_tax = soup.find("table", class_="table table-striped").\
+            find("th", text="Price (incl. tax)").nextSibling.text  # Extraction du prix TTC du livre
+        price_excl_tax = soup.find("table", class_="table table-striped").\
+            find("th", text="Price (excl. tax)").nextSibling.text  # Extraction du prix HT du livre
 
-    availability = soup.find_all("td")[-2].text  # Extraction du nombre d'exemplaires disponible
-    number_available = availability[10:12]  # Formattage de la string pour avoir un nombre
+        availability = soup.find_all("td")[-2].text  # Extraction du nombre d'exemplaires disponible
+        number_available = availability[10:12]  # Formattage de la string pour avoir un nombre
 
-    product_description = soup.find("article", class_="product_page").find_all("p")[-1].text  # Extraction de la description du livre
+        product_description = soup.find("article", class_="product_page").find_all("p")[-1].text  # Extraction de la description du livre
 
-    category = soup.find("ul", class_="breadcrumb").find_all("a")[-1].text  # Extraction de la categorie du livre
+        category = soup.find("ul", class_="breadcrumb").find_all("a")[-1].text  # Extraction de la categorie du livre
 
-    review_rating = soup.find("p", class_="star-rating")["class"][-1] + " Out of Five"  # Extraction des reviews
+        review_rating = soup.find("p", class_="star-rating")["class"][-1] + " Out of Five"  # Extraction des reviews
 
-    image_raw = soup.find("img", alt=title)["src"]  # Extraction de l'url brut de la jaquette
-    image_url = website_url + image_raw.replace("../..", "")  # Correction du lien brut pour qvoir le bon url
+        image_raw = soup.find("img", alt=title)["src"]  # Extraction de l'url brut de la jaquette
+        image_url = website_url + image_raw.replace("../..", "")  # Correction du lien brut pour qvoir le bon url
 
-    product_page_url = base_url
+        product_page_url = base_url
 
-    print(image_url)
+        info = {"product_page_url": product_page_url, "universal_product_code": upc, "title": title,
+                "price_including_tax": price_incl_tax, "price_excluding_tax": price_excl_tax,
+                "number_available": number_available, "product_description": product_description, "category": category,
+                "review_rating": review_rating, "image_url": image_url}
+        result.append(info)
+        df = pd.DataFrame(result, columns=["product_page_url", "universal_product_code", "title",
+                                           "price_including_tax", "price_excluding_tax", "number_available",
+                                           "product_description", "category", "review_rating", "image_url"])
+        df.to_csv("book_info.csv", mode="w", encoding="utf-8", index=False)
+
+    else:
+        print(r.status_code)
+
+
 
 
 if __name__ == "__main__":
