b9422c23d230a21ad7146f508da5c59feeaab73e
Cellier-Auffret Jordan <jcellierauffret@lilo.org>
2020-11-18 15:26:22 +0100

    Fonction get_books_url est terminé, besoin de trouver une alternative a la creation de 51 fichier.

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ category_list = []
 
 
 def get_books_info():
-    """ Fonction qui va chercher toutes les infos spécifique à un livre"""
+    """
+    Fonction qui va chercher toutes les infos spécifique à un livre.
+    """
     r = requests.get(base_url)  # Requests de la page du livre
     if r.status_code == 200:  # Si la page est accessible alors on continue
         result = []  # Creation d'une liste vide pour y ajouter les infos du livres plus tard
@@ -81,14 +83,46 @@ def get_books_url():
     Fonction qui va chercher les urls de chaque livre par categorie.
     :return:
     """
+    # book_links = []
+    links = []
     trial_url = "http://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
+    website_url = "http://books.toscrape.com"
     global category_list
     # print(category_list)
-    for cat in category_list:
-        url = cat["cat url"]
-        name = cat["cat name"]
-        r = requests.get(url)
-        print(url)
+    for key in category_list:
+        url = key["cat url"]
+        name = key["cat name"]
+        next_url = url
+        while True:
+            # print(name)
+            r = requests.get(next_url)
+            # print(next_url)
+            if r.status_code == 200:
+                soup = BeautifulSoup(r.content, "lxml")
+                lis = soup.find_all("li", class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
+                # print(lis)
+                for li in lis:
+                    link = li.find("a")["href"]
+                    # print(link)
+                    fixed_link = link.replace('../../..', "")
+                    # print(fixed_link)
+                    links.append('http://books.toscrape.com/catalogue' + fixed_link)
+                next_button_text = soup.find('div', class_="col-sm-8 col-md-9").find_all('a')[-1].text
+                # print(next_button_text)
+
+                if next_button_text == "next":
+                    next_button = soup.find("li", class_="next").find("a", href=True)
+                    next_url = url + "/" + next_button["href"]
+                    # print(next_url)
+                else:
+                    df = pd.DataFrame(links)
+                    # df.to_csv("Url_" + name + ".csv", encoding="utf-8", index=False, header=False)
+                    links = []
+                    # print(df1)
+                    break
+    print(df)
+
+
 
 
 if __name__ == "__main__":
